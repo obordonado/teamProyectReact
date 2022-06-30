5ebd53edc395a771c6825e0dfbda22c83d79a55d
Oscar <obordonado@protonmail.com>
2022-06-30 18:46:49 +0200

    inserted Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,28 @@
 import { useState } from 'react'
 import './App.css'
+import  { BrowserRouter, Routes, Route} from 'react-router-dom';
+import  Header from "./components/Header/Header";
+import Home from "./containers/Home/Home";
+import Login from "./containers/User/Login/Login";
+import Register from "./containers/User/Register/Register";
+import Profile from "./containers/User/Profile/Profile";
 
 function App() {
 
   return (
     <div className="App">
+      <BrowserRouter>
+        <Header />
 
-
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/films" element={<Login />} />
+          <Route path="/users/login" element={<Register />} />
+          <Route path="/users/adduser" element={<Profile />} />
+        </Routes>
+      </BrowserRouter>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import "./Header.css";
+import { Nav } from 'react-bootstrap';
+
+
+
+
+
+function Header() {
+  return (
+    <div className='Nav'>
+
+        <Nav
+        activeKey="/home"
+        onSelect={(selectedKey) => alert(`selected ${selectedKey}`)}
+        >
+        <Nav.Item>
+            <Nav.Link href="/home">Active</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+            <Nav.Link eventKey="link-1">Link</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+            <Nav.Link eventKey="link-2">Link</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+            <Nav.Link eventKey="disabled" disabled>
+            Disabled
+            </Nav.Link>
+        </Nav.Item>
+        </Nav>
+
+
+    </div>
+  )
+}
+
+export default Header

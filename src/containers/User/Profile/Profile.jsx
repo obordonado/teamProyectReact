a67837048941149450567a520aee396387142fa9
Oscar <obordonado@protonmail.com>
2022-06-30 18:18:06 +0200

    basic structure

---
+++ src/containers/User/Profile/Profile.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import "./Profile.css";
+function Profile() {
+  return (
+    <div className='Profile'>Profile</div>
+  )
+}
+
+export default Profile;
---
+++ src/containers/User/Register/Register.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import "./Register.css";
+function Register() {
+  return (
+    <div className='Register'>Register</div>
+  )
+}
+
+export default Register;

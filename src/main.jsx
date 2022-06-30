5f369018b9a05ceb2f68f99c6add9d4b95ef759d
claudia <claudiachristensen1@gmail.com>
2022-06-30 19:47:13 +0200

    Creating Provider

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import store from './app/store'
+import {Provider} from 'react-redux'
 import './index.css'
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 )

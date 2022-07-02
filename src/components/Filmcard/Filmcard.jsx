3f222e4939df5b6b9162142c8cec03ce526224c1
claudia <claudiachristensen1@gmail.com>
2022-07-02 21:36:58 +0200

    Extracted filmcard to component

--- src/components/Filmcard/Filmcard.jsx
+++ src/components/Filmcard/Filmcard.jsx
@@ -1,15 +1,28 @@
 import React from "react";
+import { Card, Button } from 'react-bootstrap'
 import './Filmcard.scss'
 
 const Filmcard = props => { 
-
+    const { director, title, year, image, genre, recomended_age, duration, price } = props;
     return (
-        <div className="Filmcard">
-            <p>Titulo: {props.data.name}</p>
-            <p>Sinopsis: {props.data.stateSky.description}</p>
-            <p>Ranking: </p>
-            {props.children}
-        </div>
+        <Card>
+            <Card.Img variant="top" src={image || "https://static.vecteezy.com/system/resources/previews/000/665/794/large_2x/vector-cinema-elements.jpg"} />
+            <Card.Body>
+                <Card.Title>{title}</Card.Title>
+                <Card.Text>
+                    <h4>Información</h4>
+                        <ul>
+                        <li>Director: {director}</li>
+                        <li>Año de estreno: {year}</li>
+                        <li>Genero: {genre}</li>
+                        <li>Edad recomendada: {recomended_age}</li>
+                        <li>Duración: {duration} minutos</li>
+                        <li>Precio: {price} EUR </li>
+                        </ul>
+                </Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+            </Card.Body>
+        </Card>
     )
 }
 
--- src/containers/Film/Film.jsx
+++ src/containers/Film/Film.jsx
@@ -1,9 +1,10 @@
 import { useEffect } from "react"
 import axios from 'axios'
 import { useDispatch, useSelector } from "react-redux"
-import { Card, Container, Row, Col, Button } from 'react-bootstrap'
+import { Container, Row, Col } from 'react-bootstrap'
 import { selectFilm, addFilmData, removeCard } from "./filmSlice"
 import Filmcard from '../../components/Filmcard/Filmcard'
+import './Film.scss'
 
 //import './Tiempo.scss'
 
@@ -34,26 +35,9 @@ const Film = props => {
         <Container className="Film">
             <Row>
                 {
-                    films.map((film, index)=> (
+                    films.map((data, index)=> (
                         <Col key={index} xs={12} md={6} xl={3}>
-                            <Card>
-                                <Card.Img variant="top" src={film.image || "https://static.vecteezy.com/system/resources/previews/000/665/794/large_2x/vector-cinema-elements.jpg"} />
-                                <Card.Body>
-                                    <Card.Title>{film.title}</Card.Title>
-                                    <Card.Text>
-                                        <h4>Información</h4>
-                                         <ul>
-                                            <li>Director: {film.director}</li>
-                                            <li>Año de estreno: {film.year}</li>
-                                            <li>Genero: {film.genre}</li>
-                                            <li>Edad recomendada: {film.recomended_age}</li>
-                                            <li>Duración: {film.duration} minutos</li>
-                                            <li>Precio: {film.price} EUR </li>
-                                         </ul>
-                                    </Card.Text>
-                                    <Button variant="primary">Go somewhere</Button>
-                                </Card.Body>
-                            </Card>
+                            <Filmcard {...data} />
                         </Col>
                     ))
                 }

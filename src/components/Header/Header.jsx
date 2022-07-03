f714a12752b9b8ec89317badc161bea5e7938f2c
Alexlg <Alejandrolg86@gmail.com>
2022-07-03 10:55:27 +0200

    adding active link on header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,19 +7,19 @@ import { NavLink } from "react-router-dom"
 function Header() {
     return (
         <div className='Nav'>
-                <Nav.Item>
-                    <NavLink className="navLink" to="/">Home</NavLink>
-                </Nav.Item>
-                <Nav.Item>
-                    <NavLink className="navLink" to="/users/login">Login</NavLink>
-                </Nav.Item>
-                <Nav.Item>
-                    <NavLink className="navLink" to="/users/adduser">Register</NavLink>
-                </Nav.Item>
-                <Nav.Item>
-                    <NavLink className="navLink" to="/films">Films</NavLink>
-                </Nav.Item>
-                {/* <Nav.Item>
+            <Nav.Item className='firstNav'>
+                <NavLink className={({ isActive }) => (isActive ? "active" : "inactive")} to="/">Home</NavLink>
+            </Nav.Item>
+            <Nav.Item>
+                <NavLink className={({ isActive }) => (isActive ? "active" : "inactive")}  to="/users/login">Login</NavLink>
+            </Nav.Item>
+            <Nav.Item>
+                <NavLink className={({ isActive }) => (isActive ? "active" : "inactive")}  to="/users/adduser">Register</NavLink>
+            </Nav.Item>
+            <Nav.Item>
+                <NavLink className={({ isActive }) => (isActive ? "active" : "inactive")}  to="/films">Films</NavLink>
+            </Nav.Item>
+            {/* <Nav.Item>
                     <Nav.Link eventKey="disabled" disabled>
                         Disabled
                     </Nav.Link>

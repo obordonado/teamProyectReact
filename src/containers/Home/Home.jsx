8854634a79e1e76d8d84cb8aa099ee2049172d3d
Alejandro <102535865+Alexdck@users.noreply.github.com>
2022-07-03 10:15:01 +0200

    Update Home.jsx

--- src/containers/Home/Home.jsx
+++ src/containers/Home/Home.jsx
@@ -1,8 +1,35 @@
 import React from 'react';
+import { Carousel } from 'react-bootstrap';
+
 import "./Home.scss";
+
 function Home() {
   return (
-    <div className='Home'>Home</div>
+    <div className='Home'>
+      <Carousel className='carousel'>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="https://i.pinimg.com/originals/0b/13/d7/0b13d709e63b00caf944d1ab3385df9d.jpg"
+            alt="Gladiator"
+          />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="https://wallpaperaccess.com/full/891207.jpg"
+            alt="Interstellar"
+          />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="https://m.media-amazon.com/images/M/MV5BMTU5NTA0OTIxNF5BMl5BanBnXkFtZTcwNjI4MTc4Mw@@._V1_.jpg"
+            alt="Matrix"
+          />
+        </Carousel.Item>
+      </Carousel>
+    </div>
   )
 }
 

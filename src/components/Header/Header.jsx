2a3b1cd19572ed6ff1a518c5aaae15722c24c746
Alejandro <102535865+Alexdck@users.noreply.github.com>
2022-07-04 21:00:29 +0200

    Update Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,31 +2,52 @@ import React from 'react';
 import "./Header.scss";
 import { Nav } from 'react-bootstrap';
 import { NavLink } from "react-router-dom"
+import { useDispatch, useSelector } from 'react-redux';
+import { logOut, userData } from '../../containers/User/userSlice';
 
 
-function Header() {
-    return (
-        <div className='Nav'>
-            <Nav.Item className='firstNav'>
-                <NavLink className={({ isActive }) => (isActive ? "active" : "inactive")} to="/">Home</NavLink>
-            </Nav.Item>
-            <Nav.Item>
-                <NavLink className={({ isActive }) => (isActive ? "active" : "inactive")}  to="/users/login">Login</NavLink>
-            </Nav.Item>
-            <Nav.Item>
-                <NavLink className={({ isActive }) => (isActive ? "active" : "inactive")}  to="/users/adduser">Register</NavLink>
-            </Nav.Item>
-            <Nav.Item>
-                <NavLink className={({ isActive }) => (isActive ? "active" : "inactive")}  to="/films">Films</NavLink>
-            </Nav.Item>
-            {/* <Nav.Item>
-                    <Nav.Link eventKey="disabled" disabled>
-                        Disabled
-                    </Nav.Link>
-                </Nav.Item> */}
+const Header = () => {
 
-        </div>
-    )
+    const identification = useSelector(userData)
+
+    const dispatch = useDispatch()
+
+    if (!identification?.name) {
+
+        return (
+            <div className='Nav1'>
+                <Nav.Item className='firstNav'>
+                    <NavLink className={({ isActive }) => (isActive ? "active" : "inactive")} to="/">Home</NavLink>
+                </Nav.Item>
+                <Nav.Item>
+                    <NavLink className={({ isActive }) => (isActive ? "active" : "inactive")} to="/films">Films</NavLink>
+                </Nav.Item>
+                <Nav.Item>
+                    <NavLink className={({ isActive }) => (isActive ? "active" : "inactive")} to="/users/login">Login</NavLink>
+                </Nav.Item>
+                <Nav.Item>
+                    <NavLink className={({ isActive }) => (isActive ? "active" : "inactive")} to="/users/adduser">Register</NavLink>
+                </Nav.Item>
+            </div>
+        )
+    } else { 
+        return (
+            <div className='Nav2'>
+                <Nav.Item className='firstNav'>
+                    <NavLink className={({ isActive }) => (isActive ? "active" : "inactive")} to="/">Home</NavLink>
+                </Nav.Item>
+                <Nav.Item>
+                    <NavLink className={({ isActive }) => (isActive ? "active" : "inactive")} to="/films">Films</NavLink>
+                </Nav.Item>
+                <Nav.Item>
+                    <NavLink className="" to="/">{identification?.user.name}</NavLink>
+                </Nav.Item>
+                <Nav.Item>
+                    <NavLink className={({ isActive }) => (isActive ? "active" : "inactive")} onClick={()=>dispatch(logOut())} >Logout</NavLink>
+                </Nav.Item>
+            </div>
+        )
+    }
 }
 
 export default Header

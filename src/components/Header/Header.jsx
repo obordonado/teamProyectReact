840a26d244dee4a119af9e3cbca12c74c28a2472
Alexlg <Alejandrolg86@gmail.com>
2022-07-05 19:55:53 +0200

    updating header styles

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -19,29 +19,28 @@ const Header = () => {
     if (!identification?.token) {
 
         return (
-            <div className='Nav1'>
-
-                <div className="nav" onClick={() => travel("/")}>Home</div>
-
-                <div className="nav" onClick={() => travel("/films")}>Films</div>
-
-                <div className="nav" onClick={() => travel("/users/login")}>Login</div>
-
-                <div className="nav" onClick={() => travel("/users/adduser")}>Register</div>
-
+            <div className='nav1'>
+                <div className="nav1First">
+                    <div className="nav" onClick={() => travel("/")}>Home</div>
+                    <div className="nav" onClick={() => travel("/films")}>Films</div>
+                </div>
+                <div className="nav1Second">
+                    <div className="nav" onClick={() => travel("/users/login")}>Login</div>
+                    <div className="nav" onClick={() => travel("/users/adduser")}>Register</div>
+                </div>
             </div>
         )
     } else {
         return (
-            <div className='Nav2'>
-                <div className="nav" onClick={() => travel("/")}>Home</div>
-
-                <div className="nav" onClick={() => travel("/films")}>Films</div>
-
-                <div className="nav" to="/">{identification?.user.name}</div>
-
-                <div className="nav" onClick={() => dispatch(logOut())} >Logout</div>
-
+            <div className='nav2'>
+                <div className='nav2First'>
+                    <div className="nav" onClick={() => travel("/")}>Home</div>
+                    <div className="nav" onClick={() => travel("/films")}>Films</div>
+                </div>
+                <div className="nav2Second">
+                    <div className="nav" to="/">{identification?.user.name}</div>
+                    <div className="nav" onClick={() => dispatch(logOut())} >Logout</div>
+                </div>
             </div>
         )
     }

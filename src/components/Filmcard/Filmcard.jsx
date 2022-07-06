53e99a1ba0ee330922446c4359d76b355f086db1
Alexlg <Alejandrolg86@gmail.com>
2022-07-06 19:02:44 +0200

    updating card styles

--- src/components/Filmcard/Filmcard.jsx
+++ src/components/Filmcard/Filmcard.jsx
@@ -2,25 +2,24 @@ import React from "react";
 import { Card, Button } from 'react-bootstrap'
 import './Filmcard.scss'
 
-const Filmcard = props => { 
+const Filmcard = props => {
     const { director, title, year, url, genre, recomended_age, duration, price, trailerUrl } = props;
     return (
-        <Card className = "Filmcard">
+        <Card className="Filmcard" style={{ width: '22rem' }}>
             <Card.Img className="cardImg" variant="top" src={url} />
-            <Card.Body>
+            <Card.Body className="card2">
                 <Card.Title>
                     <h2>{title}</h2>
                 </Card.Title>
-                <h5>Información</h5>
-                <ul>
-                    <li><strong>Director:</strong> {director}</li>
-                    <li><strong>Año de estreno:</strong> {year}</li>
-                    <li><strong>Genero:</strong> {genre}</li>
-                    <li><strong>Edad recomendada:</strong> {recomended_age}</li>
-                    <li><strong>Duración:</strong> {duration} minutos</li>
-                    <li><strong>Precio:</strong> {price} EUR </li>
+                <ul className="list">
+                    <li><strong></strong> {director}</li>
+                    <li><strong></strong> {year}</li>
+                    <li><strong></strong> {genre}</li>
+                    <li><strong></strong> {recomended_age} +</li>
+                    <li><strong></strong> {duration} minutos</li>
+                    <li><strong></strong> {price} EUR </li>
                 </ul>
-                <Button variant="primary" className ="cardBtn" size="lg" href={trailerUrl} target="_blank">Ver Trailer</Button>
+                <Button variant="primary" className="cardBtn" size="lg" href={trailerUrl} target="_blank">Ver Trailer</Button>
             </Card.Body>
         </Card>
     )

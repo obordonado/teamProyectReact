62d0fa7988b0137c30c55b2c5420f5c0c1db9976
Alexlg <Alejandrolg86@gmail.com>
2022-07-06 18:07:30 +0200

    header/home styles update

--- src/containers/Home/Home.jsx
+++ src/containers/Home/Home.jsx
@@ -6,29 +6,37 @@ import "./Home.scss";
 function Home() {
   return (
     <div className='Home'>
-      <Carousel className='carousel'>
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src="https://i.pinimg.com/originals/0b/13/d7/0b13d709e63b00caf944d1ab3385df9d.jpg"
-            alt="Gladiator"
-          />
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src="https://wallpaperaccess.com/full/891207.jpg"
-            alt="Interstellar"
-          />
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src="https://m.media-amazon.com/images/M/MV5BMTU5NTA0OTIxNF5BMl5BanBnXkFtZTcwNjI4MTc4Mw@@._V1_.jpg"
-            alt="Matrix"
-          />
-        </Carousel.Item>
-      </Carousel>
+      <div className='homeTop'></div>
+      <div className='homeBot'>
+        <div className="botLeft"></div>
+        <div className="botMid">
+
+          <Carousel className='carousel' indicators={false} controls={false}>
+            <Carousel.Item>
+              <img
+                className="d-block w-100"
+                src="https://i.pinimg.com/originals/0b/13/d7/0b13d709e63b00caf944d1ab3385df9d.jpg"
+                alt="Gladiator"
+              />
+            </Carousel.Item>
+            <Carousel.Item>
+              <img
+                className="d-block w-100"
+                src="https://wallpaperaccess.com/full/891207.jpg"
+                alt="Interstellar"
+              />
+            </Carousel.Item>
+            <Carousel.Item>
+              <img
+                className="d-block w-100"
+                src="https://m.media-amazon.com/images/M/MV5BMTU5NTA0OTIxNF5BMl5BanBnXkFtZTcwNjI4MTc4Mw@@._V1_.jpg"
+                alt="Matrix"
+              />
+            </Carousel.Item>
+          </Carousel>
+        </div>
+        <div className="botRight"></div>
+      </div>
     </div>
   )
 }

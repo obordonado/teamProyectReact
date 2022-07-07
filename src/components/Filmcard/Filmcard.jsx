fcab27ecb6eecee790af7a01813277837a217562
Alexlg <Alejandrolg86@gmail.com>
2022-07-07 20:22:14 +0200

    updating filmcard styles

--- src/components/Filmcard/Filmcard.jsx
+++ src/components/Filmcard/Filmcard.jsx
@@ -21,8 +21,9 @@ const Filmcard = props => {
 
     return (
         <>
-        <Card className = "Filmcard">
+        <Card className = "FilmCard">
             <Card.Img
+              className="filmCardImg"
               variant="top"
               src={url || "https://static.vecteezy.com/system/resources/previews/000/665/794/large_2x/vector-cinema-elements.jpg"}
               onClick={handleShow}
@@ -38,18 +39,18 @@ const Filmcard = props => {
           <Modal.Header closeButton>
             <Modal.Title>{title}</Modal.Title>
           </Modal.Header>
-          <Modal.Body>
-            <img src={url || "https://static.vecteezy.com/system/resources/previews/000/665/794/large_2x/vector-cinema-elements.jpg"}/>
+          <Modal.Body className="modalBody">
+            <img className="cardImgClick" src={url || "https://static.vecteezy.com/system/resources/previews/000/665/794/large_2x/vector-cinema-elements.jpg"}/>
             <h5>Información</h5>
             <ul>
                 <li><strong>Director:</strong> {director}</li>
-                <li><strong>Año de estreno:</strong> {year}</li>
-                <li><strong>Genero:</strong> {genre}</li>
-                <li><strong>Edad recomendada:</strong> {recomended_age}</li>
-                <li><strong>Duración:</strong> {duration} minutos</li>
-                <li><strong>Precio:</strong> {price} EUR </li>
+                <li><strong>Release date:</strong> {year}</li>
+                <li><strong>Genre:</strong> {genre}</li>
+                <li><strong>Recommended age:</strong> {recomended_age}</li>
+                <li><strong>Duration:</strong> {duration} minutes</li>
+                <li><strong>Price:</strong> {price} EUR </li>
             </ul>
-            <Button variant="primary" className ="cardBtn" size="lg" href={trailerUrl} target="_blank">Ver Trailer</Button>
+            <Button variant="primary" className ="cardBtn" size="lg" href={trailerUrl} target="_blank">Watch Trailer</Button>
           </Modal.Body>
         </Modal>
       </>
